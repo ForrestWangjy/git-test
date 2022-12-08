5966cf143bc2b0c4a73afc3ee40a4afe04b28064
forrestwang <forrest.wang@yamibuy.com>
2022-12-08 17:29:32 +0800

    seTest

--- test/src/Test.java
+++ test/src/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git commit test");
         System.out.println("git new branch");
-        System.out.println("git slow");
+        System.out.println("git fast");
     }
 }
