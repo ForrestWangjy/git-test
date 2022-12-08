add47194a3784b59e324c775dcab3bd7c7f296ca
forrestwang <forrest.wang@yamibuy.com>
2022-12-08 17:12:03 +0800

    git new branch

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,5 +3,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Test {
     public static void main(String[] args) {
         System.out.println("git commit test");
+        System.out.println("git new branch");
     }
 }
