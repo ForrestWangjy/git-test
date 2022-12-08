62ef1f984a142dbaf55c5099472d93eae89d926e
forrestwang <forrest.wang@yamibuy.com>
2022-12-08 16:58:00 +0800

    test

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("git commit test");
+    }
 }
